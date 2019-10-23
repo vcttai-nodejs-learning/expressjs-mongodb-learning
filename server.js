503bce4624a67304c27710414b377be0b4e85887
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-23 17:39:54 +0700

    135. Handling Requests and Responses of Request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//Routing
+app.get('/', function(req, res) {
+    console.log(req);
+    res.send("<h1>Hello world!</h1>");
+})
+
 app.listen(3000, () => {
     console.log('Start server successfully!');
 });
