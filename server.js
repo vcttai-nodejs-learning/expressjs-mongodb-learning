610e139ff6ea16278caa071e39bb56ee9cfef48b
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-23 22:53:38 +0700

    141 - BMI calculator

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ app.get('/', function(req, res) {
 })
 
 app.post('/', function(req, res) {
-    const num1 = parseInt(req.body.first_number);
-    const num2 = parseInt(req.body.second_number);
+    const num1 = Number(req.body.first_number);
+    const num2 = Number(req.body.second_number);
     
     res.send(`${num1} + ${num2} = ${num1 + num2}`);
+})
+
+app.get('/bmicalculation', function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculation.html");
+})
+
+app.post('/bmicalculation', function(req, res) {
+    const weight = Number(req.body.weight);
+    const height = Number(req.body.height);
+    
+    res.send(`Your BMI is: ${weight / (height * height)}`);
 })
 
 app.listen(3000, () => {
