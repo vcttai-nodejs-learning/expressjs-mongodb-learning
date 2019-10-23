9e7a344aaff2785895d8cf15cd63bc3eadc840df
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-23 22:41:54 +0700

    140 - Handle form data with body-parser

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Routing
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', function(req, res) {
+    const num1 = parseInt(req.body.first_number);
+    const num2 = parseInt(req.body.second_number);
+    
+    res.send(`${num1} + ${num2} = ${num1 + num2}`);
 })
 
 app.listen(3000, () => {
