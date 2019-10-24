5b132d2a279f5f133cc0a28c8c78bd0a91611193
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-24 11:19:19 +0700

    154. Setting Up the Newsletter Page - Static folder

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 //Routing
 app.get('/', function(req, res) {
     res.send("<h1>Hello world!</h1>");
 })
 
-app.get('/about', function(req, res) {
-    res.send("Contact me at: vcttai@gmail.com");
+app.get('/signin', function(req, res) {
+    res.sendFile(__dirname + "/signin.html");
 })
 
 app.listen(3000, () => {
