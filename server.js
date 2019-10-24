02c84044ad82cde286231324359b7505ffe286c0
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-24 10:48:49 +0700

    152 - Add parameters for API

--- server.js
+++ server.js
@@ -14,14 +14,23 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
     const crypto = req.body.crypto;
     const fiat = req.body.fiat;
+    const amount = req.body.amount;
 
-    const url = `https://apiv2.bitcoinaverage.com/indices/global/ticker/${crypto}${fiat}`;
-    request(url, function(error, response, body){
+    const options = {
+        url: 'https://apiv2.bitcoinaverage.com/convert/global',
+        method: 'GET',
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+    request(options, function(error, response, body){
         const data = JSON.parse(body);
-        const price = data.last;
+        const price = data.price;
 
-        res.write("<p>The current date is: " + data.display_timestamp + "</p>");
-        res.write(`<h1>The current price of ${crypto} is ${price}${fiat}.</h1>`);
+        res.write("<p>The current date is: " + data.time + "</p>");
+        res.write(`<h1>${amount}${crypto} is ${price}${fiat}.</h1>`);
         res.send();
     })
 })
