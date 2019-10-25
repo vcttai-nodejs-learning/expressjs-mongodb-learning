3848bf43baf42ff2c4661f51e0de4af92bfa95e6
Vu Cong Tan Tai <t000662@PC-000662-3.local>
2019-10-25 18:14:12 +0700

    225 226 - Create and save a document with mongoose

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+// Mongoose
+const dbUrl = 'mongodb://localhost:27017/mongoose_practice';
+mongoose.connect(dbUrl, {useUnifiedTopology: true, useNewUrlParser: true});
 
 //Routing
 app.get('/', function(req, res) {
     res.send("<h1>Hello world!</h1>");
 })
 
-app.get('/about', function(req, res) {
-    res.send("Contact me at: vcttai@gmail.com");
+app.get('/create_a_document', function(req, res) {
+    const fruitSchema = new mongoose.Schema({
+        name: String,
+        rating: Number,
+        review: String
+    });
+    
+    const Fruit = mongoose.model('Fruit', fruitSchema); //mongoose sẽ tự động dùng "fruits" cho tên của Collection trong DB
+    const anApple = new Fruit({
+        name: "Apple",
+        rating: 7,
+        review: "Very delicious"
+    });
+    
+    anApple.save();
+
+    res.send("Created a fruit");
 })
 
 app.listen(3000, () => {
